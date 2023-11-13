3bf568067cb8c3a05d623f6d78c7b3e7bcb6e4d4
Benny Shi <740004544@qq.com>
2023-11-14 00:22:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pytesseract
 from pdf2image import convert_from_path
 from PIL import ImageOps
 from PyPDF2 import PdfReader
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+import os
 
 def get_num_pages(pdf_path):
     reader = PdfReader(pdf_path)
@@ -10,24 +12,31 @@ def get_num_pages(pdf_path):
 
 def process_pages(pdf_path, start_page, end_page):
     images = convert_from_path(pdf_path, dpi=250, first_page=start_page, last_page=end_page)
-    text = ""
+    text_pages = {}
 
-    for image in images:
+    for i, image in enumerate(images, start=start_page):
         gray_image = ImageOps.grayscale(image)
-        text += pytesseract.image_to_string(gray_image, lang='chi_sim+eng') + "\n"
-    print(text)    
-    return text
+        text = pytesseract.image_to_string(gray_image, lang='chi_sim+eng')
+        print(f"\nPage {i} Text:\n{text}")  # Print recognized text
+        text_pages[i] = text + "\n"
+
+    return text_pages
 
 def extract_text_from_pdf(pdf_path, num_pages, batch_size=5):
-    all_text = ""
-    with ThreadPoolExecutor(max_workers=8) as executor:  # Adjust max_workers based on your CPU
+    all_text_pages = {}
+    max_workers = os.cpu_count() or 4  # Use the number of CPU cores or default to 4
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
         futures = []
         for start_page in range(1, num_pages + 1, batch_size):
             end_page = min(start_page + batch_size - 1, num_pages)
             futures.append(executor.submit(process_pages, pdf_path, start_page, end_page))
 
-        for future in futures:
-            all_text += future.result()
+        for future in tqdm(as_completed(futures), total=len(futures), desc="Processing Pages"):
+            all_text_pages.update(future.result())
+
+    # Sort the pages and concatenate the text
+    all_text = "".join(all_text_pages[i] for i in sorted(all_text_pages))
 
     return all_text
 
